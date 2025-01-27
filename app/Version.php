99d5225d2edc464cae8054d053a72ae2ed24fc18
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-01-27 13:02:09 +0300

    Add files via upload

---
+++ app/Version.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Этот файл является частью приложения GearMagic.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+namespace App;
+
+/**
+ * Версия приложения.
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package App
+ */
+class Version extends \Gm\Version\AppVersion
+{
+    /**
+     * {@inheritdoc}
+     */
+    public string $code = 'GM CMS';
+
+    /**
+     * {@inheritdoc}
+     */
+    public string $name = 'GearMagic: Site management';
+
+    /**
+     * {@inheritdoc}
+     */
+    public string $originalName = 'GearMagic: Управление сайтом';
+
+    /**
+     * {@inheritdoc}
+     */
+    public string $number = '1.0';
+
+    /**
+     * {@inheritdoc}
+     */
+    public string $date = '01/01/2025';
+
+    /**
+     * {@inheritdoc}
+     */
+    public string $resource = 'https://apps.gearmagic.ru/cms';
+
+    /**
+     * {@inheritdoc}
+     */
+    public string $docsResource = 'https://docs.gearmagic.ru/cms/1.x';
+
+    /**
+     * {@inheritdoc}
+     * 
+     * В результате обновления приложения, здесь свойству будут указаны параметры 
+     * обновленной версии редакции приложения.
+     */
+    protected mixed $edition = null;
+}
