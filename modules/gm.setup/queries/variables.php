33dd045121388c32d60bb6cb859629a9b9899bd4
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-01-31 14:08:15 +0300

    Добавлен variables.php

---
+++ modules/gm.setup/queries/variables.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Идентификаторы (переменные) запроса.
+ * 
+ * Применяются в качестве ключей при наполнении таблиц (см. queries.php).
+ * 
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+ /** @var array $vars */
+$vars = array_merge_recursive(
+    // «GearMagic: Система управления» (GM MS)
+    require('variables/ms.php'),
+    // «GearMagic: Управление сайтом» (GM CMS)
+    require('variables/cms.php'),
+    // Редакция «Стандарт» (GM CMS:STD)
+    require('variables/cms-std.php')
+);
+
+// смещение всех идентификаторов
+foreach ($vars as $name => &$items) {
+    array_unshift($items, '');
+    unset($items[0]);
+    $items = array_flip($items);
+}
+return $vars;
