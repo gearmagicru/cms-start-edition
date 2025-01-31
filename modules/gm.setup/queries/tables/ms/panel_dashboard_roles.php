69b82d26015bbf612510684db0b2639a7f042fc2
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-01-31 14:11:00 +0300

    ms

---
+++ modules/gm.setup/queries/tables/ms/panel_dashboard_roles.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Панель управления.
+ * 
+ * Файл записей таблицы "{{panel_dashboard_roles}}" (Доступ к рабочим столам Информационной панели).
+ * 
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    [
+        'panel_id' => $dashboards['desktop'], 
+        'role_id'  => $roles['root']
+    ]
+];
