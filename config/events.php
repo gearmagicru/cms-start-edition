980288fcf8a5dd17002e3fe617699cc5d2fd5f08
Anton Tivonenko <93121899+gearmagicru-ti@users.noreply.github.com>
2025-03-21 12:47:26 +0300

    Файл конфигурации слушателей событий

---
+++ config/events.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Файл конфигурации слушателей событий (модулей, расширений модулей, виджетов).
+ * 
+ * События слушателей добавленные через Панель управления "GM Panel" будут в файле 
+ * конфигурации ".events.so" (включая события перечисленные ниже).
+ * 
+ * Пример объявления событий слушателей:
+ * ```
+ * return [
+ *     'onFooBarEvent' => [
+ *         ['widgetId', 'widget'],
+ *         // ...
+ *         ['moduleId', 'module'],
+ *         // ...
+ *         ['extensionId', 'extension'],
+ *         // ...
+ *     ],
+ *     // ...
+ * ];
+ * ```
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+ 
+return [
+];
