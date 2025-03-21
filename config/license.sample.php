b7a9d675f8331c1860e2d301eee02578f650eeed
Anton Tivonenko <93121899+gearmagicru-ti@users.noreply.github.com>
2025-03-21 12:23:46 +0300

    Лицензионный ключ

---
+++ config/license.sample.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Лицензионный ключ.
+ * 
+ * Здесь указывается лицензионный ключ редакции вашего веб-приложения.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return '{key}';
