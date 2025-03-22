31cfc0f20bc25381e6b78aa7afec841d6f86d8aa
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-03-22 13:59:26 +0300

    Таблицы используемые картой SQL запросов (раздел "insert")

---
+++ .install/gm.setup/queries/tables/cms-str.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Таблицы используемые картой SQL запросов (раздел "insert").
+ * 
+ * Веб-приложения «GearMagic: Управление сайтом» (GM СMS).
+ * Редакции «Старт» (GM CMS:STR).
+ * 
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    // панель разделов
+    '{{panel_partitionbar}}' => require('cms-str/panel_partitionbar.php'),
+        // модули панели разделов
+        '{{panel_partitionbar_modules}}' => require('cms-str/panel_partitionbar_modules.php')
+];
